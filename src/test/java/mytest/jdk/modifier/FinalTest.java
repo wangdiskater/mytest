720c0a2638b4d75f003c54cb2a1ecf5e7ace0521
wangdi <wangdi@yaeher.com>
2021-05-17 19:54:49 +0800

    final test

--- src/test/java/mytest/jdk/modifier/FinalTest.java
+++ src/test/java/mytest/jdk/modifier/FinalTest.java
@@ -1,6 +1,12 @@
 package mytest.jdk.modifier;
 
+import org.bouncycastle.asn1.cms.PasswordRecipientInfo;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description
@@ -9,6 +15,8 @@ import org.junit.jupiter.api.Test;
  * @date 2021-05-17 14:18
  */
 public class FinalTest {
+    private final Map finalMap =  new HashMap();
+    private final List finalList =  new ArrayList();
 
     /**
      * 局部变量final 只能设置一次值
@@ -18,6 +26,15 @@ public class FinalTest {
         final int a;
         a = 100;
         System.out.println(a);
+    }
+
+    /**
+     * 可以修改指向的内容
+     */
+    @Test
+    void finalTest2(){
+        finalMap.put("a","b");
+        finalList.add("a");
     }
 
 
